945c4b2f898246eba66d1e69b8423071c7f8e9dd
Gbolahan <molubode81@gmail.com>
2023-01-08 14:07:35 +0100

    Admin profile setup

--- controllers/donor.controller.js
+++ controllers/donor.controller.js
@@ -8,8 +8,6 @@ exports.getProfile = (req, res, next) => {
         .populate('donation')
         .then((user) => {
             const donations = user.donation
-            // const date = JSON.stringify(user.donation[0].date).slice(15)
-            // console.log('date ',date);
             res.render('donorProfile', {
                 donor: user,
                 donations: donations,
@@ -39,10 +37,6 @@ exports.notifyAdmin = (req, res, next) => {
             // TODO: Nodemailer Notify Admin
 
             res.redirect('/donor/profile')
-            // .status(200).json({
-            //     status: 'Success!',
-            //     message: 'Kindly wait while admin verify your donation. Thanks!'
-            // })
         }).catch((error) => {
             console.log('Here: ', error)
             error.httpStatusCode = 500
@@ -56,10 +50,6 @@ exports.signup = (req, res) => {
     const { user } = req
     console.log(user);
     res.redirect(307, '/profile')
-    // res.status(200).render('donorProfile', {
-    //     donor: user,
-    //     docTitle: user.firstname + user.firstname
-    // })
 }
 
 exports.login = (req, res) => {
